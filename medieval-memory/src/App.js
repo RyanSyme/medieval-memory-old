01de723e20c8b4d155885d61f0e454056a92ccbe
RyanSyme <ryu321@hotmail.com>
2021-12-16 10:16:24 +0000

    changed all images

--- medieval-memory/src/App.js
+++ medieval-memory/src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  {"src": "/img/helmet-1.png", matched: false},
-  {"src": "/img/potion-1.png", matched: false},
-  {"src": "/img/ring-1.png", matched: false},
-  {"src": "/img/scroll-1.png", matched: false},
-  {"src": "/img/shield-1.png", matched: false},
-  {"src": "/img/sword-1.png", matched: false}
+  {"src": "/img/helmet.png", matched: false},
+  {"src": "/img/potion.png", matched: false},
+  {"src": "/img/ring.png", matched: false},
+  {"src": "/img/scroll.png", matched: false},
+  {"src": "/img/shield.png", matched: false},
+  {"src": "/img/sword.png", matched: false}
 ]
 
 function App() {
